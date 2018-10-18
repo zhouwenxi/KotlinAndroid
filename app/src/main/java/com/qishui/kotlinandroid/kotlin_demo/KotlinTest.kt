48c32e733abe27f4a5241c28c570e12b1d08ae8c
zhouwenxi <qishuichixi@126.com>
2018-10-18 11:06:46 +0800

    添加依赖库设置

---
+++ app/src/main/java/com/qishui/kotlinandroid/kotlin_demo/KotlinTest.kt
@@ -0,0 +1,13 @@
+package com.qishui.kotlinandroid.kotlin_demo
+
+/**
+ * @author Zhou
+ * Created on 2018/10/18 10:56.
+ * Email:qishuichixi@163.com
+ * Desc:  测试Kotlin模拟器
+ */
+fun main(args: Array<String>) {
+
+    print("Hello Kotlin !")
+
+}
